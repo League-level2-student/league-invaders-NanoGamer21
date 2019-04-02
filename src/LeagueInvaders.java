bd17ba0769187717414095f7d53aada93fb1ce8a
308824 <308824@0338S-S-R0B6H0L.sdcs.local>
2019-04-02 14:27:57 -0700

    step 2

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,16 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	int height = 800;
+	int width = 500;
+	public static void main(String[] args) {
+		new LeagueInvaders().createUI();
+	}
+		private void createUI(){
+			frame = new JFrame();
+			frame.setSize(width, height);
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 }
